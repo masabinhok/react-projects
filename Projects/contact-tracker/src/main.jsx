0bf421adc805af6d913581c544de42dd6fd9a20d
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-06-23 00:00:50 +0545

    global pending UI checkpoint

---
+++ Projects/contact-tracker/src/main.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contacts";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import { loader as contactLoader } from "./routes/contacts";
+import EditContact, {action as editAction} from "./routes/edit";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "/contacts/:contactId",
+        element: <Contact />,
+        errorElement: <ErrorPage />,
+        loader: contactLoader,
+      },
+      {
+        path: "/contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
