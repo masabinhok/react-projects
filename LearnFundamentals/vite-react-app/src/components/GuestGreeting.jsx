61acaa608c061934bc2e457246e3499019206276
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-06-30 18:22:40 +0545

    keep projects only

--- LearnFundamentals/vite-react-app/src/components/GuestGreeting.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function GuestGreeting() {
-  return <h1>Please sign up!</h1>;
-}
-
-
--- LearnFundamentals/vite-react-app/src/components/UserGreeting.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function UserGreeting(props) {
-  return <h1>Welcome back {props.name}</h1>;
-}
